a68b792699ee0561189b0d01f947c0410f8462de
kriti <kkumari@55-ip.com>
2021-08-10 12:35:57 +0530

    Fixed static file serve

--- server/scripts/server.sh
+++ server/scripts/server.sh
@@ -4,5 +4,10 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-python manage.py migrate
-daphne -b 0.0.0.0 -p 8000 shore.asgi:application
+# for production
+#daphne -b 0.0.0.0 -p 8000 shore.asgi:application
+
+
+# only for dev deployments
+python manage.py collectstatic --noinput
+python manage.py runserver "0.0.0.0:8000"
